a50450f0882658fda827515e7422ef400c25c557
Dmitrii Sobin <neo7976@yandex.ru>
2023-03-31 12:43:54 +0300

    Teams - Создание таблиц и настройка связи

--- src/main/resources/db/migration/V1__Initial_db.sql
+++ src/main/resources/db/migration/V1__Initial_db.sql
@@ -10,9 +10,9 @@ values ('Как дела товарищи????');
 
 create table users
 (
-    id                 bigserial primary key,
-    name               varchar(1024),
-    telephone_number varchar(1024) unique
+    id               bigserial primary key,
+    name             varchar(20),
+    telephone_number varchar(14) unique
 );
 
 insert into users (name, telephone_number)
@@ -27,3 +27,26 @@ insert into users (name, telephone_number)
 values ('Ольга', '+79100212565');
 insert into users (name, telephone_number)
 values ('Даниил', '+79999856587');
+
+
+create table teams
+(
+    id        bigserial primary key,
+    title     varchar(255) unique,
+    depiction varchar(1024),
+    logo      varchar(1024) unique
+);
+
+CREATE TABLE teamMembers
+(
+    team_id INT,
+    user_id INT,
+    PRIMARY KEY (team_id, user_id),
+    FOREIGN KEY (team_id) REFERENCES teams (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+
+INSERT INTO teams (id, title, depiction) VALUES (1, 'Team A', 'Команда из Рязани');
+INSERT INTO teamMembers (team_id, user_id) VALUES (1, 1);
+INSERT INTO teamMembers (team_id, user_id) VALUES (1, 2);
